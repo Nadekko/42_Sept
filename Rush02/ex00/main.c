f663d50ac4003a8b38f8257f9b8edac5fcfa8b5c
Nadekko <139667516+Nadekko@users.noreply.github.com>
2023-09-23 17:48:26 +0200

    Add files via upload

--- Rush02/ex00/main.c
+++ Rush02/ex00/main.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
-#include <fcntf.h>
+#include <fcntl.h>
 
 int	main(int ac, char **av)
 {
 	int	fd;
+	long long num;
+	char	*dict;
 
-	fd = open("./numbers.dict", O_RDWR)
-	if (fd == NULL)
+	fd = open("./numbers.dict", O_RDWR);
+	if (fd == -1)
 		ft_putstr("CAN'T OPEN DICT\n");
+		return (1);
 	if (ac < 2 || ac > 3)
 	{
 		ft_putstr("Error\n");
@@ -15,34 +18,20 @@ int	main(int ac, char **av)
 	}
 	if (ac == 2)
 	{
-		if (ft_atoi(av[1]) < 0)
-		{
-			ft_putstr("Error\n");
-			return (1);
-		}
-		//call fonction dict and print array
+		dict = "numbers.dict";
+		num = ft_atoi(av[1]);
 	}
-	if (ac == 3)
+	else
 	{
-		if (ac == 1)
-		{
-			if (ft_atoi(av[1]) < 0)
-			{
-				ft_putstr("Error\n");
-				return (1);
-			}
-			else
-			{
-				//fputs = (av[1], fd); (print array in dict)
-
-		}
-		if (ac == 2)
-		{
-			//ajout de la valeur de nbr)
-		}
+		num = ft_atoi(av[2]);
+		dict = av[1];
 	}
+	if (num < 0)
+	{
+		ft_putstr("Error\n");
+		return (1);
+	}
+
 	close (fd);
 	return (0);
 }
-
-
